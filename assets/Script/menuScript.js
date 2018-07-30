830ccabe53ebb1ba10db31004001210985afc881
Jesse-Cameron <jessec.cameron@gmail.com>
2018-07-30 15:21:38 +1000

    updated menu scene

---
+++ assets/Script/menuScript.js
@@ -0,0 +1,16 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        console.log(this.node)
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
