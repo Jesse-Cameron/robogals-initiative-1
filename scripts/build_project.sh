033509b96b98c7ed1b5cf32b9843bc27c979f697
Jesse <jessec.cameron@gmail.com>
2018-08-04 15:48:14 +1000

    updated mount to avoid T&C

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-curl -O http://digitalocean.cocos2d-x.org/CocosCreator/v1.10.0/CocosCreator_v1.10.0_20180730.dmg
-sudo hdiutil attach CocosCreator_v1.10.0_20180730.dmg
+COCOS_DMG="CocosCreator_v1.10.0_20180730.dmg"
+COCOS_MNT="cocos_mount"
+
+# attach and install cocos
+curl -O http://digitalocean.cocos2d-x.org/CocosCreator/v1.10.0/$COCOS_DMG
+hdiutil convert $COCOS_DMG -format UDTO -o cocos.cdr
+hdiutil attach -noautoopen -mountpoint $COCOS_MNT cocos.cdr
+
+# set the cocos language
+cat ~/.CocosCreator/settings.json
+
+# build the damn file
+$COCOS_MNT/CocosCreator.app/Contents/MacOS/CocosCreator --path ../ --build "platform=web-desktop"
+
+
+# detach and remove
+rm -rf $COCOS_DMG
+hdiutil detach $COCOS_MNT
