a7af2751d99930ebe3350c892cc824b9a609cfc0
Jesse <jessec.cameron@gmail.com>
2018-08-11 11:41:22 +1000

    added force push

--- scripts/gh_pages_deploy.sh
+++ scripts/gh_pages_deploy.sh
@@ -16,14 +16,14 @@ mkdir ../tmp
 mv ./build/web-desktop/* ../tmp/
 ls -al ..
 ls -al ../tmp
-# commit
 git checkout -b gh-pages
 rm -rf ./* # remove all of the files
 mv ../tmp/* ./ # add in the build files
 ls -al
 pwd
 
+# commit
 git add .
 git commit -m "deploy $version"
 git remote add origin-pages https://${GH_TOKEN}@github.com/Jesse-Cameron/robogals-initiative-1.git
-git push --quiet --set-upstream origin-pages gh-pages
+git push --quiet --set-upstream --force origin-pages gh-pages
