9abbf2262c4ad22c1de4fcabebe175fa29ae6fec
Jesse <jessec.cameron@gmail.com>
2018-08-12 10:43:26 +1000

    moved deploy to travis stage

--- scripts/gh_pages_deploy.sh
+++ scripts/gh_pages_deploy.sh
@@ -4,23 +4,16 @@ set -ex
 
 version=$(jq '.version' package.json)
 
-# set ssh key
+# set git account
 git config --local user.name "Travis Deploy"
 git config --local user.email "travis@travis-ci.org"
-git status
-
 
 # add all the build files
-ls
 mkdir ../tmp
-mv ./build/web-desktop/* ../tmp/
-ls -al ..
-ls -al ../tmp
+mv ./build/web-desktop/* ../tmp/ # move files to a temp directory
 git checkout -b gh-pages
 rm -rf ./* # remove all of the files
 mv ../tmp/* ./ # add in the build files
-ls -al
-pwd
 
 # commit
 git add .
