93a0630365d3cac0370540234c92d3449e444bd4
Jesse-Cameron <jessec.cameron@gmail.com>
2018-08-18 12:52:13 +1000

    added letter event handling

--- assets/Script/menuScript.js
+++ assets/Script/menuScript.js
@@ -7,10 +7,18 @@ cc.Class({
   // use this for initialization
   onLoad() {
     console.log(this.node);
+    this.S_node = this.node.getChildByName("label_S");
+    setupEventHandlers(this);
   },
 
   // called every frame
   update() {
 
-  }
+  },
 });
+
+const setupEventHandlers = (that) => {
+  that.S_node.on('mousedown', () => {
+    console.log('Oi! You just touched me!');
+  });
+}
