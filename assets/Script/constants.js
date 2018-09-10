6fe750ed36ce1f950ebefce8d0e30b139ef0d20b
Jesse-Cameron <jessec.cameron@gmail.com>
2018-09-10 21:33:58 +1000

    added tech scene

--- assets/Script/constants.js
+++ assets/Script/constants.js
@@ -1,2 +1,5 @@
-exports.SCI_NARRATIVE_1_SCENE = 'sci-narrative-1';
 exports.MENU_SCENE = 'menu-scene';
+exports.SCI_NARRATIVE_1_SCENE = 'sci-narrative-1';
+
+exports.TECH_NARRATIVE_1_SCENE = 'tech-narrative-1';
+
