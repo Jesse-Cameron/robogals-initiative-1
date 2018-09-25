ef520e3118b0424ccb3690983c163318c91deb0b
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-09-25 20:34:05 +0800

    less dodgy block performance
    Ensures that nextX is within max and min bounds

--- assets/Script/technology/techGame1.js
+++ assets/Script/technology/techGame1.js
@@ -9,41 +9,26 @@
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
 
 const setupEventHandlers = (that) => {
-    that.block1.on('touchstart', (startEvent) => {
-        that.block1.on('touchmove', () => {
-            console.log(startEvent.touch);
-            var deltaX = startEvent.touch.getDelta().x;    // get touch movement in x axis
-            console.log(deltaX);
+    that.block1.on('touchmove', (moveEvent) => {
+        if(moveEvent.getID() != 0)
+            return;
 
-            var blockWidth = that.block1.width;
-            var blockX = that.block1.position.x;
-            var blockY = that.block1.position.y;
+        const halfWidth = that.block1.width/2
+        const frameWidth = cc.view.getFrameSize().width;   
+        const blockX = that.block1.position.x; // convert block coordinates
+        const deltaX = moveEvent.getDelta().x;
+        var nextX = blockX + deltaX;
 
-            var frameWidth = cc.view.getFrameSize().width;
-            var frameLeftX = frameWidth/2 * -1;         // get frame's left x coordinate
-            var frameRightX = frameWidth/2;             // get frame's right x coordinate
-            
-            var nextX = blockX + deltaX;                    // next x coord for block
-            var newPosition  = cc.v2(nextX, blockY);        // position vector
-
-            var blockLeft = nextX - blockWidth/2;            // block's left edge Xcoord
-            var blockRight = nextX + blockWidth/2;           // block's right edge Xcoord
-
-            if(blockLeft <= frameLeftX){  // if block tries to move past left edge
-                newPosition = cc.v2(frameLeftX + blockWidth/2, blockY); // create v2 type
-                console.log("HITLEFT " + " newposition: "+ newPosition + " frameLeftX: " + frameLeftX + " blockWidth/2: " + blockWidth/2);
-
-            }
-            else if(blockRight >= frameRightX){ // if block tries to move past right edge
-                newPosition = cc.v2(frameRightX - blockWidth/2, blockY); // create v2 type
-                console.log("HITRight " + " newposition: "+ newPosition + " frameRightX: " + frameRightX + " blockWidth/2: " + blockWidth/2);
-            }
-            else{
-                console.log("Normal " + " newposition: "+ newPosition + " deltaX: " + deltaX + " blockX: " + blockX + " blockWidth/2: " + blockWidth/2);
-            }
-            var blockAction = cc.moveTo(0, newPosition); 
-            that.block1.runAction(blockAction);   
-        });
+        const limit = frameWidth/2-halfWidth;
+        if(nextX < 0){
+            nextX = Math.max(nextX, -limit);
+        }
+        if (nextX > 0){
+            nextX = Math.min(nextX, limit);
+        }
+        const blockAction = cc.moveTo(0, cc.v2(nextX, that.block1.position.y)); 
+        that.block1.runAction(blockAction); 
+        // }
     });
 
   };
