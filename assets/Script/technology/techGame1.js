c96f6a6245acd17d9e006aeaf262e092eed18e0d
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-09-29 14:09:11 +0800

    techGame1.js resolve merge conflict

--- assets/Script/technology/techGame1.js
+++ assets/Script/technology/techGame1.js
@@ -33,19 +33,30 @@ const setupEventHandlers = (that) => {
   });
 };
 
+const { FADE_TIME } = require('../constants');
+
 cc.Class({
   extends: cc.Component,
 
   properties: {
   },
 
+  // LIFE-CYCLE CALLBACKS:
   onLoad() {
+    this.node.opacity = 0;
+    this.node.color = new cc.Color(0, 0, 0);
+    this.node.runAction(
+      cc.fadeIn(FADE_TIME)
+    );
+
+    // touch input block
     this.block1 = this.node.getChildByName('block1');
     setupEventHandlers(this);
-  },
-
-  start() {
   }
+
+  // start() {
+
+  // },
 
   // update (dt) {},
 });
