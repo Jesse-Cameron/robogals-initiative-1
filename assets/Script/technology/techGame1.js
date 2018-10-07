a473920f895bce3b76912f746dbfb8aba18bba7c
Jesse-Cameron <jessec.cameron@gmail.com>
2018-10-07 14:19:58 +1100

    fix movement bugs

--- assets/Script/technology/techGame1.js
+++ assets/Script/technology/techGame1.js
@@ -6,17 +6,17 @@ const limit = (x) => {
   return Math.tanh(1 / maxMove * x) * maxMove;
 };
 
-const bufferSize = 50; // margin between block and edge of screen
-const frameWidth = cc.view.getFrameSize().width;
-
 const setupEventHandlers = (that) => {
+  const bufferSize = 50; // margin between block and edge of screen
+  const frameWidth = cc.view.getFrameSize().width;
+  const halfBlockWidth = that.block1.width / 2;
+
   that.block1.on('touchmove', (moveEvent) => {
     if (moveEvent.getID() !== 0) {
       return;
     }
 
     const blockX = that.block1.position.x; // convert block coordinates
-    const blockWidth = that.block1.width;
 
     const deltaX = moveEvent.getDelta().x;
     const nextX = blockX + limit(deltaX);
@@ -24,16 +24,15 @@ const setupEventHandlers = (that) => {
 
     const worldX = that.block1.parent.convertToWorldSpaceAR(newPosition).x;
 
-    if (worldX > (frameWidth - (blockWidth / 2) - bufferSize)) {
+    if (worldX > (frameWidth - halfBlockWidth - bufferSize)) {
       that.block1.emit('touchend');
       return;
     }
 
-    if (worldX < ((blockWidth / 2) + bufferSize)) {
+    if (worldX < (halfBlockWidth + bufferSize)) {
       that.block1.emit('touchend');
       return;
     }
-    // console.log(`delta ${deltaX}, activated: ${limit(deltaX)}`);
 
     const blockAction = cc.moveTo(0, newPosition);
     that.block1.runAction(blockAction);
