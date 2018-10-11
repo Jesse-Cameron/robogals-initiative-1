fc227e55ccfbd13e3f422c6579e31c27b1833e97
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-10-11 19:31:33 +0800

    add left right movement to falling blocks
    block still falls during touch event

--- assets/Script/technology/techGame1.js
+++ assets/Script/technology/techGame1.js
@@ -34,9 +34,42 @@ const setupEventHandlers = (that) => {
   });
 };
 
+const setupBlockEventHandler = (newBlock) => {
+  const bufferSize = 50; // margin between block and edge of screen
+  const frameWidth = cc.view.getFrameSize().width;
+  const halfBlockWidth = newBlock.width / 2;
+
+  newBlock.on('touchmove', (moveEvent) => {
+    if (moveEvent.getID() !== 0) {
+      return;
+    }
+
+    const blockX = newBlock.position.x;
+    const deltaX = moveEvent.getDelta().x;
+    const nextX = blockX + limit(deltaX);
+    const newPosition = cc.v2(nextX, newBlock.position.y);
+    const worldX = newBlock.parent.convertToWorldSpaceAR(newPosition).x; // convert block to world coordinates
+
+    if (worldX > (frameWidth - halfBlockWidth - bufferSize)) {
+      newBlock.emit('touchcancel');
+      return;
+    }
+
+    if (worldX < (halfBlockWidth + bufferSize)) {
+      newBlock.emit('touchcancel');
+      return;
+    }
+
+    const blockAction = cc.moveTo(0, newPosition);
+    newBlock.runAction(blockAction);
+  });
+}
 const createFallingBlock = (that) => {
   const newBlock = cc.instantiate(that.blockPrefab);
   that.node.addChild(newBlock);
+  
+  setupBlockEventHandler(newBlock);
+  
   const worldX = (Math.random() * (cc.view.getFrameSize().width - 200)) + 400;
   const spawnX = newBlock.convertToNodeSpace(cc.v2(worldX, that.generatedBlockY)).x;
   newBlock.setPosition(cc.v2(spawnX, that.generatedBlockY));
@@ -58,6 +91,7 @@ cc.Class({
     this.node.runAction(
       cc.fadeIn(FADE_TIME)
     );
+    this.block = this.node.getChildByName('block');
     setupEventHandlers(this);
   },
 
