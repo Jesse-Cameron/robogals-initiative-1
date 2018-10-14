e2f8283020d4608e06cf750010c9caa1f3f88eda
Jesse-Cameron <jessec.cameron@gmail.com>
2018-10-14 20:51:27 +1100

    added blockmovement to fallingBlock.js

--- assets/Script/game/fallingBlock.js
+++ assets/Script/game/fallingBlock.js
@@ -1,8 +1,89 @@
+/**
+ * Limits the x value to a certain size. Using tanh as an activation function
+ *
+ * @param {int} x - the integer you want to limit
+ * @returns {int} - new limited x
+ */
+const limit = (x) => {
+  const maxMove = 100;
+  return Math.tanh(1 / maxMove * x) * maxMove;
+};
+
+/**
+ *
+ * @param {cc.Event.EventTouch} event - movement event
+ */
+const movementEventHandler = ({
+  that,
+  moveEvent,
+  frameWidth,
+  halfBlockWidth,
+  bufferSize
+}) => {
+  if (moveEvent.getID() !== 0) {
+    return;
+  }
+
+  const blockX = that.node.position.x;
+  const deltaX = moveEvent.getDelta().x;
+  const nextX = blockX + limit(deltaX);
+  const newPosition = cc.v2(nextX, that.node.position.y);
+  const worldX = that.node.parent.convertToWorldSpaceAR(newPosition).x; // convert block to world coordinates
+
+  if (worldX > (frameWidth - halfBlockWidth - bufferSize)) {
+    that.node.emit('touchcancel');
+    return;
+  }
+
+  if (worldX < (halfBlockWidth + bufferSize)) {
+    that.node.emit('touchcancel');
+    return;
+  }
+
+  that.movingVertical = false;
+  const blockAction = cc.moveTo(0, newPosition);
+  if (that.movingHorizontal) {
+    that.node.runAction(blockAction);
+  }
+};
+
+const setupEventHandler = (that) => {
+  const bufferSize = 50; // margin between block and edge of screen
+  const frameWidth = cc.view.getFrameSize().width;
+  const halfBlockWidth = that.node.width / 2;
+
+  that.node.on('touchmove', (moveEvent) => {
+    movementEventHandler({
+      that,
+      moveEvent,
+      bufferSize,
+      frameWidth,
+      halfBlockWidth
+    });
+  });
+
+  that.node.on('touchstart', () => {
+    that.movingHorizontal = true;
+  });
+
+  that.node.on('touchcancel', () => {
+    that.movingVertical = true;
+    that.movingHorizontal = false;
+  });
+
+  that.node.on('touchend', () => {
+    that.movingVertical = true;
+    that.movingHorizontal = false;
+  });
+};
+
 cc.Class({
   extends: cc.Component,
 
   properties: {
-    speed: 200
+    speed: 50,
+    movingHorizontal: true,
+    movingVertical: true
   },
 
   // LIFE-CYCLE CALLBACKS:
@@ -15,6 +96,8 @@ cc.Class({
       }
       sp.spriteFrame = spriteFrame;
     });
+
+    setupEventHandler(this);
   },
 
   start() {
@@ -22,7 +105,9 @@ cc.Class({
   },
 
   update(dt) {
-    this.node.y -= this.speed * dt;
+    if (this.movingVertical) {
+      this.node.y -= this.speed * dt;
+    }
   }
 
   // this code is currently unused as we have nothing to collide with !
