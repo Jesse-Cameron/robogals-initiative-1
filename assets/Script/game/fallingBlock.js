5ad5fb4414baa5a1f2e95e193952602753514a8e
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-10-14 19:17:24 +0800

    add collision detection to falling and catcher blocks

--- assets/Script/game/fallingBlock.js
+++ assets/Script/game/fallingBlock.js
@@ -23,10 +23,31 @@ cc.Class({
 
   update(dt) {
     this.node.y -= this.speed * dt;
-  }
+  },
 
   // this code is currently unused as we have nothing to collide with !
-  // onCollisionEnter(other, self) {
+  onCollisionEnter: function (other, self) {
+    console.log('on collision enter');
+    self.node.destroy();
 
-  // }
+    // Collider Manager will calculate the value in world coordinate system, and put them into the world property
+    var world = self.world;
+
+    // Collider Component aabb bounding box
+    var aabb = world.aabb;
+
+    // The position of the aabb collision frame before the node collision
+    var preAabb = world.preAabb;
+
+    // world transform
+    var t = world.transform;
+
+    // Circle Collider Component world properties
+    var r = world.radius;
+    var p = world.position;
+
+    // Rect and Polygon Collider Component world properties
+    var ps = world.points;
+  },
+
 });
